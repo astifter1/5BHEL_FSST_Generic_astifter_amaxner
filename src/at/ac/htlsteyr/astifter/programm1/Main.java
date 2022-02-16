6578472e4105bbaf78abcc31e854d16728308f90
astifter <astifter@htl-steyr.ac.at>
2022-02-16 10:43:17 +0100

    Hinzufügen von Stackoverflow und Stackunderflow exceptions

--- src/at/ac/htlsteyr/astifter/programm1/Main.java
+++ src/at/ac/htlsteyr/astifter/programm1/Main.java
@@ -33,12 +33,44 @@ class Stack<T> {
     }
 
     void push(T value) {
+
+        try {
+            if(stackpointer >= 10) {
+                throw new Stackoverflow();
+            }
+
+        } catch (Stackoverflow stackoverflow) {
+            stackoverflow.printStackTrace();
+        }
         array[stackpointer] = value;
         stackpointer++;
+
     }
 
     T pull(){
+
+
+        try {
+            if(stackpointer == 0){
+                throw new Stackunderflow();
+            }
+        } catch (Stackunderflow stackunderflow) {
+            stackunderflow.printStackTrace();
+        }
+
         return array[stackpointer];
     }
 
+}
+
+class Stackoverflow extends Exception{
+    public Stackoverflow(){
+        System.out.println("Stackoverflow Error");
+    }
+}
+
+class Stackunderflow extends Exception{
+    public Stackunderflow(){
+        System.out.println("Stackunderflow Error");
+    }
 }
